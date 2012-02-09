da1b2cfb94f8356df0e6fd814e5a4f32e1eabc0d
unknown <Glouton@Glouton-laptop.(none)>
2012-02-09 16:25:11 +0100

    Mise en place de l'interface admin django-grapppelli

--- www/Front/admin.py
+++ www/Front/admin.py
@@ -2,6 +2,8 @@
 
 from django.contrib import admin
 from models import Menu, Item
+from django.contrib.auth.models import User, Group
+from django.contrib.sites.models import Site
 
 class MenuAdmin(admin.ModelAdmin):
 	list_display	= ("name", "text", "price")
@@ -16,3 +18,6 @@ class ItemAdmin(admin.ModelAdmin):
 	
 admin.site.register(Menu, MenuAdmin)
 admin.site.register(Item, ItemAdmin)
+admin.site.unregister(User)
+admin.site.unregister(Group)
+admin.site.unregister(Site)
--- www/urls.py
+++ www/urls.py
@@ -16,5 +16,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
+    (r'^grappelli/', include('grappelli.urls')),
     (r'', include('Front.urls')),
 )
